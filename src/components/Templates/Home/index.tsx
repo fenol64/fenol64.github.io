2848eaa13ce9d53e209b6f3b208d55b29f37c2da
fenol64 <contato@fenol64.com.br>
2025-09-30 03:30:13 -0300

    projects

--- src/components/Templates/Home/index.tsx
+++ src/components/Templates/Home/index.tsx
@@ -1,6 +1,7 @@
 import AboutMeSection from "@/components/organism/AboutMeSection";
 import Header from "@/components/organism/Header";
 import HeroSection from "@/components/organism/HeroSection";
+import ProjectsSection from "@/components/organism/ProjectsSection";
 import { getTranslations } from "next-intl/server";
 import React, { FC } from "react";
 
@@ -10,11 +11,13 @@ const THome: FC = async () => {
   const t = await getTranslations("HomePage");
   const tHero = await getTranslations("HomePage.hero");
   const tAboutMe = await getTranslations("AboutMeSection");
+  const tProjects = await getTranslations("ProjectsSection");
   return (
     <>
       <Header title={t("navbar.title")} items={t.raw("navbar.items")} />
       <HeroSection t={tHero} />
       <AboutMeSection t={tAboutMe} />
+      <ProjectsSection t={tProjects} />
     </>
   );
 };
