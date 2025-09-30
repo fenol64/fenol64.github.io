575ec1e1ac1f320b8b803ff789f544f1c91135a4
fenol64 <contato@fenol64.com.br>
2025-09-30 03:07:10 -0300

    no warnings

--- src/components/molecules/HeroProfile/index.tsx
+++ src/components/molecules/HeroProfile/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Badge from '@/components/atoms/Badge';
+import Image from 'next/image';
 
 interface HeroProfileProps {
   t: any;
@@ -10,11 +11,13 @@ const HeroProfile: React.FC<HeroProfileProps> = ({ t }) => (
     <div className="absolute -top-4 right-0">
       <Badge>{t('badges.years')}</Badge>
     </div>
-    <img
-      src="/profile.png"
-      alt="Profile"
-      className="w-56 h-56 rounded-full shadow-lg object-cover border-4 border-white"
-    />
+    <Image
+        src="/profile.png"
+        alt="Profile"
+        width={224}
+        height={224}
+        className="w-56 h-56 rounded-full shadow-lg object-cover border-4 border-white"
+      />
     <div className="absolute -bottom-5 left-0">
       <Badge color="bg-blue-500">{t('badges.stack')}</Badge>
     </div>
